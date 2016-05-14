46706249e4fa7b3fc33fce352ed1053747115856
Sascha Feldmann <sascha.feldmann@gmx.de>
2016-05-14 18:26:07 +0200

    #1 prepared maven packaging, wrote first unittest for build pipeline testing

---
+++ import/src/main/java/de/saschafeldmann/adesso/master/thesis/software/Importer.java
@@ -0,0 +1,20 @@
+package de.saschafeldmann.adesso.master.thesis.software;
+
+/**
+ *
+ * Project:        Masterthesis of Sascha Feldmann
+ * Creation date:  14.05.2016
+ * Author:         Sascha Feldmann (sascha.feldmann@gmx.de)
+ *
+ * University:
+ * Hochschule für Technik und Wirtschaft, Berlin
+ * Fachbereich 4
+ * Studiengang Internationale Medieninformatik (Master)
+ *
+ * Company:
+ * adesso AG
+ *
+ * Importer main class.
+ */
+public class Importer {
+}
