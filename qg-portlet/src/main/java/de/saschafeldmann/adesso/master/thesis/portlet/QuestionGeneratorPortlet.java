fc146539f00f574b4aa632a948636e283fa1f053
Sascha Feldmann <sascha.feldmann@gmx.de>
2016-05-23 17:12:36 +0200

    #2 added portlet API and Spring dependencies

--- qg-portlet/src/main/java/de/saschafeldmann/adesso/master/thesis/portlet/QuestionGeneratorPortlet.java
+++ qg-portlet/src/main/java/de/saschafeldmann/adesso/master/thesis/portlet/QuestionGeneratorPortlet.java
@@ -4,15 +4,19 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.navigator.Navigator;
 import com.vaadin.navigator.View;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.WrappedPortletSession;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import de.saschafeldmann.adesso.master.thesis.portlet.presenter.course.information.CourseInformationPresenter;
+import de.saschafeldmann.adesso.master.thesis.portlet.presenter.course.information.CourseInformationPresenterImpl;
 import de.saschafeldmann.adesso.master.thesis.portlet.properties.VaadinProperties;
 import de.saschafeldmann.adesso.master.thesis.portlet.properties.VersionProperties;
 import de.saschafeldmann.adesso.master.thesis.portlet.view.course.information.CourseInformationView;
 import de.saschafeldmann.adesso.master.thesis.portlet.view.course.information.CourseInformationViewImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.portlet.context.PortletApplicationContextUtils;
 
 /**
  * Project:        Masterthesis of Sascha Feldmann
@@ -33,16 +37,29 @@ import de.saschafeldmann.adesso.master.thesis.portlet.view.course.information.Co
 public class QuestionGeneratorPortlet extends UI {
     private Navigator viewNavigator;
     private CourseInformationView courseInformationView;
+    private ApplicationContext applicationContext;
+
+    private ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
+        initializeApplicationContext(vaadinRequest);
         initializeViews();
 
         setContent(courseInformationView.getRootComponent());
     }
 
+    private void initializeApplicationContext(VaadinRequest vaadinRequest) {
+        WrappedPortletSession wrappedPortletSession = (WrappedPortletSession) vaadinRequest.getWrappedSession();
+
+        this.applicationContext = PortletApplicationContextUtils.getRequiredWebApplicationContext(wrappedPortletSession.getPortletSession().getPortletContext());
+    }
+
     private void initializeViews() {
-        CourseInformationPresenter courseInformationPresenter;
+        CourseInformationPresenter courseInformationPresenter = getApplicationContext().getBean(CourseInformationPresenterImpl.class);
+
         this.courseInformationView = courseInformationPresenter.initializeView();
         this.viewNavigator.addView(CourseInformationViewImpl.VIEW_NAME, courseInformationView);
     }
